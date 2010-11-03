4c0422aa43d08251f6ede10b3d3fe13de315cd60
Rob Fisher <robfisher@gmail.com>
2010-11-03 21:25:45 +0000

    Koch learning order now defined in koch_order.py

---
+++ koch_order.py
@@ -0,0 +1,5 @@
+# after X should be <BT> but don't know how to get that in cwtext. See NOTES.
+order = "KMRSUAPTLOWI.NJEF0Y,VG5/Q9ZH38B?427C1D6X!="
+
+def getLetters(numLetters):
+    return order[0:numLetters]
