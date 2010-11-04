2efe3a5bb0838f938b31e552d1844de2cf5bc313
Rob Fisher <robfisher@gmail.com>
2010-11-04 22:54:57 +0000

    play.py no longer uses lintest.sh and mactest.sh scripts

--- play.py
+++ play.py
@@ -3,6 +3,8 @@ import os
 import platform
 
 operatingSystem = platform.system()
+speed = 20
+farnsworth = 15
 
 # TODO: call cwtext properly in a child process or something
 # that we can interactively kill before it completes
@@ -14,10 +16,15 @@ def play(words):
         playCharacters(word)
 
 def playCharacters(characters):
+    command = 'echo "' + characters + '" | cwtext-0.96/cwpcm -w ' + str(speed) + ' -F ' + str(farnsworth) + ' -lowrez | sox -b8 -u -r8000'
     if operatingSystem == "Darwin":
-        os.system("./mactest.sh " + characters)
+        command += ' -traw - -t wav tmp.wav lowpass 1500'
+        os.system(command)
+        os.system('afplay tmp.wav')
+        os.system('rm tmp.wav')
     else:
-        os.system("./lintest.sh " + characters)
+        command += ' -traw - -t raw /dev/dsp lowpass 1500'
+        os.system(command)
 
 def main(argv=None):
     if argv == None:
@@ -26,4 +33,3 @@ def main(argv=None):
 
 if __name__ == "__main__":
     sys.exit(main())
-
