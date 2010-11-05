5fafc067e0068f15f6a1688cbe5e7fe7307b6551
Rob Fisher <robfisher@gmail.com>
2010-11-05 23:02:16 +0000

    Speed, farnsworth and letters boxes in GUI

--- mkgui.py
+++ mkgui.py
@@ -6,8 +6,33 @@ class MkGui:
     def __init__(self, master):
         frame = Frame(master)
         frame.pack()
+        r=0
+        self.speedVar = IntVar()
+        self.speedLabel = Label(frame, text="Speed");
+        self.speedLabel.grid(column=0, row=r)
+        self.speedBox = Spinbox(frame, from_=5, to=40, increment=1, textvariable=self.speedVar)
+        self.speedBox.grid(column=1, row=r)
+        r = r + 1
+        self.farnsworthVar = IntVar()
+        self.farnsworthLabel = Label(frame, text="Farnsworth")
+        self.farnsworthLabel.grid(column=0, row=r)
+        self.farnsworthBox = Spinbox(frame, from_=5, to=40, increment=1, textvariable=self.farnsworthVar)
+        self.farnsworthBox.grid(column=1, row=r)
+        r = r +1
+        self.lettersVar = IntVar()
+        self.lettersLabel = Label(frame, text="Letters")
+        self.lettersLabel.grid(column=0, row=r)
+        self.lettersBox = Spinbox(frame, from_=2, to=koch.maxLetters(), increment=1, textvariable=self.lettersVar)
+        self.lettersBox.grid(column=1, row=r)
+        r = r + 1
         self.button = Button(frame, text="Start", command=self.start)
-        self.button.pack(side=RIGHT)
+        self.button.grid(column=1, row=r)
+
+        self.speedVar.set(15)
+        self.farnsworthVar.set(20)
+        self.lettersVar.set(2)
+        
+        
 
     def start(self):
         koch.startKoch(20)
